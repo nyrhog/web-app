89d3d3998c647c77aab66657ba938bfa8006295c
nyrhog <nyrhog@gmail.com>
2021-04-14 17:01:22 +0300

    Initial commit

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,9 @@
+
+O
+src/main/java/HelloServlet.java,d\3\d3c8e2a0d0182f9817133dd90320435305c69dd8
+?
+settings.gradle,0\5\05efc8b1657769a27696d478ded1e95f38737233
+<
+build.gradle,f\0\f07866736216be0ee2aba49e392191aeae700a35
+O
+src/main/java/EmptyServlet.java,3\a\3a26e1d4a945e07922f081ad365af9fb57d7ebeb
---
+++ src/main/java/EmptyServlet.java
@@ -0,0 +1,23 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/")
+public class EmptyServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter writer = response.getWriter();
+        try {
+            writer.println("<h2>Hello from HelloServlet</h2>");
+        } finally {
+            writer.close();
+        }
+
+    }
+}
